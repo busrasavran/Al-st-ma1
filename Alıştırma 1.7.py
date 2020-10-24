9c30eb0af4a3b1b13930164d01729cb2d8317d1d
busrasavran <72864078+busrasavran@users.noreply.github.com>
2020-10-24 19:59:19 +0300

    Add files via upload

---
+++ Alıştırma 1.7.py
@@ -0,0 +1,11 @@
+toplam_sayı=0
+for n in range(100,1000):
+    n=str(n)
+    if int(n[0])+int(n[1])== int(n[2]):
+        toplam_sayı=toplam_sayı+1
+        print(n)
+print(toplam_sayı)        
+
+        
+        
+    
