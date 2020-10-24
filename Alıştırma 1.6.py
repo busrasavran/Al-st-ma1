90b594d5e80253c7d42f08530a71e2e07913f392
busrasavran <72864078+busrasavran@users.noreply.github.com>
2020-10-24 19:53:33 +0300

    Add files via upload

---
+++ Alıştırma 1.6.py
@@ -0,0 +1,18 @@
+sayı_adet=0
+for n in range(1000,10000):
+    n=str(n)
+    if int(n[0])>int(n[3]):
+        sayı_adet=sayı_adet+1
+print(sayı_adet)
+        
+        
+    
+        
+    
+    
+    
+    
+        
+        
+    
+        
